c27b9b3eddf0408f3712d94da67fc6c0aa3e2dd4
ditoaryap <ditoaryaputra1324@gmail.com>
2025-10-30 21:02:48 +0700

    learn use redirect

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,8 +1,38 @@
-import { Controller, Get, Param, Post, Query, Req } from '@nestjs/common';
-import type { Request } from 'express';
+import {
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  // Param,
+  Post,
+  Query,
+  Redirect,
+  // Req,
+  // Res,
+} from '@nestjs/common';
+import type { HttpRedirectResponse } from '@nestjs/common';
+import type { Request, Response } from 'express';
 
 @Controller('/api/users')
 export class UserController {
+  @Get('/sample-response')
+  @Header('Content-Type', 'application/json')
+  @HttpCode(200)
+  sampleResponse(): Record<string, string> {
+    return {
+      data: 'test',
+    };
+  }
+
+  @Get('/redirect')
+  @Redirect()
+  redirect(): HttpRedirectResponse {
+    return {
+      url: '/api/users/sample-response',
+      statusCode: 301,
+    };
+  }
+
   @Get('/hello')
   sayHello(
     @Query('first_name') firstName: string,
@@ -13,10 +43,10 @@ export class UserController {
 
   //test
 
-  @Get('/:id')
-  getById(@Param('id') id: string): string {
-    return `GET ${id}`;
-  }
+  // @Get('/:id')
+  // getById(@Param('id') id: string): string {
+  //   return `GET ${id}`;
+  // }
 
   @Post()
   post(): string {
